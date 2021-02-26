c2b5f0eea267c486418a5c66c39eba0a74236a04
Levi Kohler <kohler.la01@gmail.com>
2021-02-26 09:58:06 -0800

    write and pass second test for Bread class

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -4,14 +4,26 @@ using Bakery.Models;
 namespace Bakery.Tests
 {
   [TestClass]
-  public class BreadTests
+  public class BreadPriceTests
   {
-    
+  
     [TestMethod]
     public void BreadConstructor_CreateInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread();
+      Bread newBread = new Bread(5);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+
+      [TestMethod]
+      public void BreadPrice_ReturnPriceOfOneLoaf_Int()
+    {
+      int breadPrice = 5;
+      Bread newBread = new Bread(breadPrice);
+
+      int result = newBread.BreadPrice;
+
+      Assert.AreEqual(breadPrice, result);
+    }
+
   }
 }
