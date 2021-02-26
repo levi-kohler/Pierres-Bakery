905e3aef1189876955e01e4091e20c04d75632c6
Levi Kohler <kohler.la01@gmail.com>
2021-02-26 15:41:03 -0800

    start UI logic with Program.cs

---
+++ Bakery/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using Bakery.Models;
+
+namespace Program
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("Today's deals are:");
+      Console.WriteLine("- Buy 2 loaves of bread and get the third free!");
+      Console.WriteLine ("- Buy 1 pastry for $2 or 3 pastries for $5!");
+
+    }
+  }
+}
